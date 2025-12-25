311194f52389501badebb7575038dee951fcc924
Ahmed-M-Aboutaleb <ahmed.aboutaleb.work@gmail.com>
2025-12-25 14:23:12 +0200

    Init

--- src/admin/routes/analytics/page.tsx
+++ src/admin/routes/analytics/page.tsx
@@ -6,7 +6,7 @@ const AnalyticsPage = () => {
   return (
     <Container className="divide-y p-0">
       <div className="flex items-center justify-between px-6 py-4">
-        <Heading level="h2">Analytics Dashboard 7</Heading>
+        <Heading level="h2">Analytics Dashboard</Heading>
       </div>
     </Container>
   );
@@ -15,7 +15,7 @@ const AnalyticsPage = () => {
 export const config = defineRouteConfig({
   label: "Analytics",
   icon: ChartBar,
-  rank: 4,
+  rank: 2,
 });
 
 export default AnalyticsPage;
